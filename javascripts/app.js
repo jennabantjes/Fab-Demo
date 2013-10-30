70e9b319b32ae61256757b38da975dbd7fc18f2e
Jenna Bantjes <jenna.bantjes@freshtilledsoil.com>
2013-10-30 17:13:25 -0400

    Add main image rotator

--- javascripts/app.js
+++ javascripts/app.js
@@ -7,5 +7,20 @@ $(document).ready(function() {
       itemsDesktopSmall : [979,3]
  
   });
+
+    $(".main-rotator").owlCarousel({
+ 
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
  
 });
