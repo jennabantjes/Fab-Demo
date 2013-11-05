f58aaea1c62fc8f1612afbdb5175eef7d2b21b9a
Jenna <jennabantjes@gmail.com>
2013-11-05 14:05:11 -0500

    Add hover nav

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,26 +1,34 @@
 $(document).ready(function() {
- 
+
   $(".owl-carousel").owlCarousel({
- 
+
       items : 4,
       itemsDesktop : [1199,3],
       itemsDesktopSmall : [979,3]
- 
+
   });
 
     $(".main-rotator").owlCarousel({
- 
+
       slideSpeed : 300,
       paginationSpeed : 400,
       singleItem:true
- 
+
       // "singleItem:true" is a shortcut for:
-      // items : 1, 
+      // items : 1,
       // itemsDesktop : false,
       // itemsDesktopSmall : false,
       // itemsTablet: false,
       // itemsMobile : false
- 
+
   });
- 
+
+    $('body').on('mouseover', '#women', function(){
+      $('.popup-nav').addClass('visible');
+      $('body').on('click', function(){
+        $('.popup-nav').removeClass('visible');
+        console.log('clicking');
+      });
+    });
+
 });
